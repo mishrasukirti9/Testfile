d7cc100901998325f1caca5024931c0a08aa7abd
mishrasukirti9 <mishrasukirti9@gmail.com>
2021-06-20 16:50:36 +0530

    Changes made

--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(130,305,300,170)
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -53,6 +55,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    platform.display();
 
     box5.display();
     log4.display();
